891abd9fa327d2f2c29f55cf54de06a53529b47f
Andrew Kiellor <akiellor@gmail.com>
2013-04-08 19:47:17 -0700

    Birthday!

---
+++ files.rb
@@ -0,0 +1,23 @@
+meta "file" do
+  accepts_value_for :source
+  accepts_value_for :target
+  accepts_value_for :owner, "root"
+  accepts_value_for :group, "root"
+  accepts_value_for :permissions, "644"
+
+  template {
+    def source_path
+      dependency.load_path.parent / source
+    end
+
+    met? { target.p.file? && target.p.owner == owner && target.p.group == group && File.stat(target).mode.to_s(8)[3..5] == permissions && source_path.read == target.p.read } 
+    meet {
+      target.p.open("w+") do |f|
+        f << source_path.read
+      end
+      shell! "chown #{owner}:#{group} #{target}"
+      shell! "chmod #{permissions} #{target}"
+    }
+  }
+end
+
